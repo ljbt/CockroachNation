2099d4c4e8027ebf95f4df74f23106d22b600fec
rafoutek <raphaelroyperso@gmail.com>
2019-02-16 19:43:21 +0100

    CHange of variables bot to cockroach

--- simulation.c
+++ simulation.c
@@ -29,35 +29,35 @@ void circle(float xCenter, float yCenter, float radius)
 // Default width and height
 #define WindowWidth 600
 #define WindowHeight 600
-#define NumberOfBots 300
+#define NumberOfCockroachs 300
 const double PowerForWeights = 3;	// To decrease or increase the influence of the distance
 const double Horizon = 300.;		// Units to look for neighbors
 const double WeightOfNeighbors = .1;
-const double Bubble = 5.;			// A minimal distance between bots
+const double Bubble = 5.;			// A minimal distance between cockroachs
 const double WeightOfEscape = .8;
 const double WeightOfMimic = .3;
 const double MimicHorizon = 30.;		// Units to look for neighbors to mimic
 const double PredatorBubble = 20.;		// A minimal distance with the predator
 const double WeightOfPredatorEscape = .8;
 
-enum {RandomWalker, SimpleBot} mode = SimpleBot /*RandomWalker*/;
+enum {RandomWalker, SimpleCockroach} mode = SimpleCockroach /*RandomWalker*/;
 
 typedef struct {
 	float x;
 	float y;
 	float speedRho;
 	float speedTheta;
-} Bot;
+} Cockroach;
 
-Bot *initializeSwarm(const int swarmSize) {
-	Bot *swarm = (Bot*)calloc(swarmSize, sizeof(Bot));
+Cockroach *initializeSwarm(const int swarmSize) {
+	Cockroach *swarm = (Cockroach*)calloc(swarmSize, sizeof(Cockroach));
 	const double GroupSpeedTheta = 0.;//valeurAleatoire()*2.*M_PI;
 	for (int i = 0; i < swarmSize; ++i) {
 		const double theta = valeurAleatoire()*2.*M_PI;
 		const double rho = sqrt(valeurAleatoire()*pow(fmin(WindowWidth/2., WindowHeight/2.), 2));
 		const double x = WindowWidth*.5+cos(theta)*rho;
 		const double y = WindowHeight*.5+sin(theta)*rho;
-		swarm[i] = (Bot){
+		swarm[i] = (Cockroach){
 			.x = x,
 			.y = y,
 			.speedRho = 1.,
@@ -67,20 +67,20 @@ Bot *initializeSwarm(const int swarmSize) {
 	return swarm;
 }
 
-void displaySwarm(const Bot *swarm, const int swarmSize) {
+void displaySwarm(const Cockroach *swarm, const int swarmSize) {
 	epaisseurDeTrait(2);
 	couleurCourante(255, 255, 255);
 	for (int i = 0; i < swarmSize; ++i)
 		point(swarm[i].x, swarm[i].y);//, 1);
 }
 
-void updateSwarm(Bot *swarm, const int swarmSize, int predatorAbscissa, int predatorOrdinate) {
+void updateSwarm(Cockroach *swarm, const int swarmSize, int predatorAbscissa, int predatorOrdinate) {
 	for (int i = 0; i < swarmSize; ++i) {	// All the individuals
 		switch (mode) {
 			case RandomWalker:
 				swarm[i].speedTheta = valeurAleatoire()*2.*M_PI;
 				break;
-			case SimpleBot:
+			case SimpleCockroach:
 				{
 					// Rule 1: avoid being alone
 					double sumX = -0.;
@@ -185,7 +185,7 @@ int main(int argc, char *argv[]) {
 
 
 void gestionEvenement(EvenementGfx event) {
-	static Bot *bot = NULL;
+	static Cockroach *cockroach = NULL;
 	static bool displayPredator = false;
 	static int predatorAbscissa = -1;
 	static int predatorOrdinate = -1;
@@ -193,11 +193,11 @@ void gestionEvenement(EvenementGfx event) {
 	switch (event) {
 		case Initialisation:
 			demandeTemporisation(20);
-			bot = initializeSwarm(NumberOfBots);
+			cockroach = initializeSwarm(NumberOfCockroachs);
 			break;
 
 		case Temporisation:
-			updateSwarm(bot, NumberOfBots, predatorAbscissa, predatorOrdinate);
+			updateSwarm(cockroach, NumberOfCockroachs, predatorAbscissa, predatorOrdinate);
 			rafraichisFenetre();
 			break;
 
@@ -206,7 +206,7 @@ void gestionEvenement(EvenementGfx event) {
 			couleurCourante(150, 0, 0);
 			if (displayPredator)
 				circle(abscisseSouris(), ordonneeSouris(), PredatorBubble);
-			displaySwarm(bot, NumberOfBots);
+			displaySwarm(cockroach, NumberOfCockroachs);
 			break;
 
 		case Clavier:
@@ -217,8 +217,8 @@ void gestionEvenement(EvenementGfx event) {
 					break;
 				case 'R':
 				case 'r':
-					free(bot);
-					bot = initializeSwarm(NumberOfBots);
+					free(cockroach);
+					cockroach = initializeSwarm(NumberOfCockroachs);
 					break;
 			}
 			break;
